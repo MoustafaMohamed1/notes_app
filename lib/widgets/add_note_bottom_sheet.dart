b9e0a9467448e9c5d440d41a53fc6396e916a201
MoustafaMohamed1 <mostafa.034569@gmail.com>
2024-02-26 23:48:49 +0200

    finish modes bottom sheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:noteapp/widgets/custom_text_field.dart';
 
+import 'custom_bottom.dart';
+
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  const Padding(
+    return const Padding(
         padding: EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
+        child: SingleChildScrollView(
+          child: Column(
           children: [
             const SizedBox(
               height: 32,
@@ -21,7 +24,9 @@ class AddNoteBottomSheet extends StatelessWidget {
               hintText: 'Content',
               maxLines: 5,
             ),
+            SizedBox(height: 32,),
+            CustomBottom(),
           ],
-        ));
+    ),),);
   }
 }
---
+++ lib/widgets/custom_bottom.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class CustomBottom extends StatelessWidget {
+  const CustomBottom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 55,
+      child: const Center(
+        child: Text('Add',style: TextStyle(color: Colors.black,fontSize: 21,fontWeight: FontWeight.bold),),
+      ),
+      decoration: BoxDecoration(
+        color: kPrimaryColor,
+        borderRadius: BorderRadius.circular(8),
+      ),
+    );
+  }
+}
