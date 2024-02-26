d4c0d0fc2ea84d94a87fc06c2951cd0f55440cb7
MoustafaMohamed1 <mostafa.034569@gmail.com>
2024-02-26 23:55:38 +0200

    create edit mode view

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:noteapp/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context){
+          return const EditNoteView();
+        }));
+      },
+      child:Container(
       padding: const EdgeInsets.only(
         top: 24,
         bottom: 24,
@@ -49,6 +56,6 @@ class NoteItem extends StatelessWidget {
             ),),
         ],
       ),
-    );
+    ));
   }
 }
